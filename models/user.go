87c6d521a406d2f7e7b3721983dff1cd610f928a
shono-ishibashi <shono.ishibashi@rakus-partners.co.jp>
2021-05-04 16:28:18 +0900

    add docker config change db from sqlite to mariadb

--- models/user.go
+++ models/user.go
@@ -8,7 +8,7 @@ type User struct {
 	gorm.Model
 	Name     string `json:"name"`
 	Email    string `json:"email"`
-	Password string `json:"-"`
+	Password string `json:"-`
 }
 
 func InsertUser(user *User) {
